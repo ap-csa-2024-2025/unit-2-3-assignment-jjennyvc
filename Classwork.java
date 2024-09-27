34eaba9d4da0b041cc08bdaf659d2c82fb9eb454
jjennyvc <nyho100>
2024-09-27 10:52:28 -0700

    skibidi toilet

--- Classwork.java
+++ Classwork.java
@@ -2,7 +2,23 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 3));
+
+  }
+  public static String firstLastN(String word, int n)
+
+  {
+
+    String output = new String();
+
+    String firstN = word.substring(0, n);
+
+    int indexOfToLast = word.length() - n;
+    String lastN = word.substring(indexOfToLast);
+
+    output = firstN + lastN;
+    return output;
+
   }
 
   // write solutions to problems below
